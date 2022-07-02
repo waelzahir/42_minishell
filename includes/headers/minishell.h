4e386d31d9d9818ecfb017265eb3b2de079d4294
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-07-02 19:17:42 +0100

    minishell's header file

--- includes/headers/minishell.h
+++ includes/headers/minishell.h
@@ -6,11 +6,19 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 17:02:50 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/24 17:02:51 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/07/02 19:13:14 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+# include<stdio.h>
+# include<stdlib.h>
+# include<stdbool.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+void	free_all(void *ptr);
 
 #endif
