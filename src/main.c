569b6264d8e37059ecaabc415498580a01bdf733
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-07-02 19:17:26 +0100

    Readline and print a prompt

--- src/main.c
+++ src/main.c
@@ -6,7 +6,20 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 17:02:26 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/06/24 17:02:27 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/07/02 19:13:53 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/headers/minishell.h"
+
+int	main(void)
+{
+	char	*cmd;
+
+	while (true)
+	{
+		cmd = readline("Minishell $ ");
+	}
+	free_all((void *) cmd);
+	return (0);
+}
