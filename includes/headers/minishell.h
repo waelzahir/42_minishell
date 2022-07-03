fad93470bfac30bf3c027292dde1ea415b8eb460
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-07-03 18:57:01 +0100

    Add header files

--- includes/headers/minishell.h
+++ includes/headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/24 17:02:50 by sel-kham          #+#    #+#             */
-/*   Updated: 2022/07/03 00:40:45 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/07/03 17:13:30 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@
 # include<stdio.h>
 # include<stdlib.h>
 # include<stdbool.h>
+# include<unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include "tokenizer.h"
 
 void	free_all_pointers(void *ptr);
 
