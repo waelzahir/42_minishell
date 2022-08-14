924826d623bff422c9aa6d4fb1104c6d8b114380
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-08-14 16:30:22 +0100

    PWD file

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -1,29 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/03 13:20:02 by ozahir            #+#    #+#             */
-/*   Updated: 2022/08/14 16:29:37 by sel-kham         ###   ########.fr       */
+/*   Created: 2022/08/14 15:49:25 by sel-kham          #+#    #+#             */
+/*   Updated: 2022/08/14 16:29:23 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "../includes/headers/minishell.h"
 
-# include <stdlib.h>
-# include <stdio.h>
-# include <unistd.h>
-# include "../libft/libft.h"
-# include "datatypes.h"
-# include "lexer.h"
-# include "parser.h"
-# include "t_stack.h"
-# include "shell.h"
-# include "binary_tree.h"
-# include <readline/history.h>
-# include <readline/readline.h>
-
-#endif
+int	ft_pwd(void)
+{
+	
+}
