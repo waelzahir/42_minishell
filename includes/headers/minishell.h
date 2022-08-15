43dbde30049a054d0804516d164c8e52c270b0c8
Soufiane Elkahmlichi <05soufiane@gmail.com>
2022-08-15 18:46:23 +0100

    Add s_shared_data struct

--- includes/headers/minishell.h
+++ includes/headers/minishell.h
@@ -6,7 +6,7 @@
 /*   By: sel-kham <sel-kham@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 13:20:02 by ozahir            #+#    #+#             */
-/*   Updated: 2022/08/14 16:29:37 by sel-kham         ###   ########.fr       */
+/*   Updated: 2022/08/15 18:21:52 by sel-kham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,13 @@
 # include "binary_tree.h"
 # include <readline/history.h>
 # include <readline/readline.h>
+
+typedef struct s_shared_data
+{
+	char	**env;
+	int		exit_status;
+}	t_sdata;
+
+extern t_sdata	data;
 
 #endif
