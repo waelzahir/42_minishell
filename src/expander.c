3449868f885345d0925d0834157c7f2b1da401d2
Ouail Zahir <ozahir@e3r4p6.1337.ma>
2022-09-07 19:19:03 +0100

    filx exit status leak

--- src/expander.c
+++ src/expander.c
@@ -43,7 +43,7 @@ char	*get_var(t_lexer *lexer, char **environ)
 	if (lexer->c == '$')
 		return (lexer_advance(lexer), ft_strdup("ma3arefch "));
 	if (lexer->c == '?')
-		return (lexer_advance(lexer), ft_strdup(ft_itoa(g_exit_stat[3])));
+		return (lexer_advance(lexer), ft_itoa(g_exit_stat[3]));
 	if (lexer->c == '\0')
 		return (char_str('$'));
 	str = char_str(lexer->c);
